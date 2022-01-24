76ef6a1c8d8694b041e2bc08f56b2b01c07bd3e4
0xflotus <0xflotus@gmail.com>
2022-01-24 17:30:05 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="hardxor",
-    version="0.0.1",
+    version="0.0.2",
     description="Xor any file byte by byte",
     long_description=README,
     long_description_content_type="text/markdown",
